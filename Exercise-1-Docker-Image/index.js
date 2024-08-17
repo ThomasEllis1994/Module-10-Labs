cc03aeed50433ac6bf8437b9a51d378993528a20
ThomasEllis1994 <ThomasGEllis004@gmail.com>
2024-08-17 13:49:22 -0400

    fixed the docker images. exposed port needs to be listening port

--- Exercise-1-Docker-Image/index.js
+++ Exercise-1-Docker-Image/index.js
@@ -1,12 +1,14 @@
+// import express from "express";
 const express = require("express");
 const app = express();
-const port = 8000;
 
 app.use(express.json());
-
+const port = 8080;
+// app.use("/", express.static("public"));
 app.use("/", (req, res) => {
     res.send({ status: 200, message: "Module-10 Expirimentation" });
 });
+//changed above for test
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
